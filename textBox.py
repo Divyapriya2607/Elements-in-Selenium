2b91d42519eded8230b60a115c73cd8d371d5e7c
Divyapriya2607 <106463594+Divyapriya2607@users.noreply.github.com>
2022-06-23 10:51:46 +0530

    Add files via upload
    Textbox completed

--- textBox.py
+++ textBox.py
@@ -21,22 +21,22 @@ try:
     for card in cards:
         card_name = card.find_element(By.XPATH, "//div/div[@class='card-body']/h5").text
         print(card_name)
-        time.sleep(5)
+        #time.sleep(5)
         if card_name == "Elements":
-            time.sleep(3)
+            #time.sleep(3)
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             card.find_element(By.XPATH, "//div/div[@class='avatar mx-auto white']").click()
-            time.sleep(3)
+            #time.sleep(3)
             print("clicked")
             menu_list = driver.find_elements(By.XPATH, "//div[@class='element-list collapse show']")
             for item in menu_list:
                 Button_textbox=item.find_element(By.XPATH, "//div[@class='element-list collapse show']/ul/li").text
-                time.sleep(3)
+                #time.sleep(3)
                 if Button_textbox == "Text Box":
-                    time.sleep(3)
+                    #time.sleep(3)
                     item.find_element(By.XPATH, "//li[@id='item-0']").click()
                     print("textbox link clicked")
-                    time.sleep(2)
+                    #time.sleep(2)
                 else:
                     print("textbox not clicked")
             textbox_username = driver.find_element(By.XPATH, "//label[text()='Full Name']/following::input[@id='userName']")
@@ -47,10 +47,13 @@ try:
             print("email")
             #WebDriverWait(driver, 5).until(
                 #EC.presence_of_element_located((By.XPATH, "//label[text()='Current Address']/following::textarea[@id='currentAddress']")))
-            textarea_address = driver.find_element(By.XPATH, "//label[text()='Current Address']/following::textarea[@id='currentAddress']")
-            textarea_address.send_keys("watson street")
+            textarea_currentaddress = driver.find_element(By.XPATH, "//label[text()='Current Address']/following::textarea[@id='currentAddress']")
+            textarea_currentaddress.send_keys("watson street")
             print("address")
-            time.sleep(2)
+            text_permanentaddress = driver.find_element(By.XPATH, "//label[text()='Permanent Address']/following::textarea[@id='permanentAddress']")
+            text_permanentaddress.send_keys("Besant nagar")
+
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             button_submit = WebDriverWait(driver, 5).until(
                 EC.presence_of_element_located((By.XPATH, "//button[@id='submit']")))
             button_submit.click()
